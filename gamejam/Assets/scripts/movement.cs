fd891389d77a704e499c7751e1588b6a971444d4
Xiaolong Dong <dongxiao@usc.edu>
2022-07-31 16:31:39 -0700

    test

--- gamejam/Assets/scripts/movement.cs
+++ gamejam/Assets/scripts/movement.cs
@@ -6,9 +6,13 @@ public class movement : MonoBehaviour
 {
     Rigidbody2D rb;
     bool canJump = false;
-    Vector2 force = new Vector2(0f, 20f);
+    
+    Vector2 vertical = new Vector2(0f, 20f);
+    Vector2 horizontal = new Vector2(0f, 0f);
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        
+        
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
@@ -16,15 +20,25 @@ public class movement : MonoBehaviour
             Debug.Log(1);
             canJump = true;
         }
+        
     }
 
 
     void Update()
     {
+        processInput();
+        
+    }
+
+    void processInput(){
         if(Input.GetKeyDown(KeyCode.Space) && canJump){
-            
-            rb.AddForce(force, ForceMode2D.Impulse);
+            vertical.x = rb.velocity.x;
+            rb.velocity = vertical;
             canJump = false;
         }
+        float xInput = Input.GetAxisRaw("Horizontal");
+        horizontal.x = xInput*10f;
+        horizontal.y = rb.velocity.y;
+        rb.velocity = horizontal;
     }
 }
