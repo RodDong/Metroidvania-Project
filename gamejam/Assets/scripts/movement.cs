2ee515ae730394ffd2ea0297dc2a2c0324e17812
JAAACKL <lzixuan@umich.edu>
2022-07-31 22:43:28 -0400

    Update movement.cs
    Clean up movement code written Rod

---
+++ gamejam/Assets/scripts/movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool canJump = false;
+    float jump_init_v = 20f;
+    
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "ground") {
+            Debug.Log(1);
+            canJump = true;
+        }
+    }
+
+
+    void Update() {
+        processInput();
+    }
+
+    void processInput() {
+        Vector2 v = rb.velocity;
+        if(Input.GetKeyDown(KeyCode.Space) && canJump) {
+            v.y = jump_init_v;
+            canJump = false;
+        }
+        v.x = Input.GetAxisRaw("Horizontal") * 10f;
+        rb.velocity = v;
+    }
+}
