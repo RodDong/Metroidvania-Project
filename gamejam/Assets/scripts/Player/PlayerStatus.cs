e3eb39bd8fcd525e6d17a2ec40649b70079ec404
JAAACKL <lzixuan@umich.edu>
2022-08-13 18:23:17 -0400

    With last commit

--- gamejam/Assets/scripts/Player/PlayerStatus.cs
+++ gamejam/Assets/scripts/Player/PlayerStatus.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
+    [SerializeField] GameObject wall;
     public bool isDetected;
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            clearArea();
+        }
+    }
+
+    public void beingDetected() {
+        isDetected = true;
+        wall.SetActive(true);
+    }
+
+    public void clearArea() {
+        isDetected = false;
+        wall.SetActive(false);
     }
 }
