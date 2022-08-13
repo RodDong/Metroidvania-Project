18cfb4fd6d56c96fb2e849c265072dbb5d0a6c2c
Xiaolong Dong <dongxiao@usc.edu>
2022-08-13 13:42:27 -0700

    updated boss script

--- gamejam/Assets/BossMovement.cs
+++ gamejam/Assets/BossMovement.cs
@@ -5,39 +5,35 @@ using UnityEngine.U2D.IK;
 public class BossMovement : MonoBehaviour
 {
     Rigidbody2D bossRigidBody;
-    [SerializeField] Transform limbSolver1 , limbSolver2;
     Vector3 solverPos1, solverPos2;
+    [SerializeField]
+    float jumpDelay = 0.2f;
     
-    // Start is called before the first frame update
     void Start()
     {
         bossRigidBody = gameObject.GetComponent<Rigidbody2D>();
-        solverPos1 = limbSolver1.localPosition;
-        solverPos2 = limbSolver2.localPosition;
 
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
+
         if(other.collider.tag == "ground"){
-            Debug.Log(limbSolver1.localPosition);
-            limbSolver1.localPosition = solverPos1;
-            limbSolver2.localPosition = solverPos2;
+            bossRigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
             gameObject.GetComponentInParent<Animator>().SetTrigger("jump");
-            bossRigidBody.AddForce(Vector2.up*20, ForceMode2D.Impulse);
-            bossRigidBody.AddForce(Vector2.left*20, ForceMode2D.Impulse);
-            
-            
+            Invoke("Jump", jumpDelay);
         }
-        // if(Input.GetKeyDown(KeyCode.A)){
-        //     gameObject.transform.Translate(Vector3.left);
-        // }
         
+    }
+
+    void Jump(){
+        bossRigidBody.constraints = RigidbodyConstraints2D.None;
+        bossRigidBody.AddForce(Vector2.up*20, ForceMode2D.Impulse);
+        bossRigidBody.AddForce(Vector2.left*30, ForceMode2D.Impulse);
     }
 }
