d21e204a2881b5576fbf4993462649ee23f36658
Yifan Jiang <PassbyKamenRider@outlook.com>
2022-09-07 23:19:26 -0500

    Change piranha collider

--- gamejam/Assets/scripts/piranha/piranha.cs
+++ gamejam/Assets/scripts/piranha/piranha.cs
@@ -6,6 +6,8 @@ public class piranha : MonoBehaviour
 {
     Vector3 startPos;
     [SerializeField] float force;
+    private Quaternion upward = Quaternion.Euler(0, 0, 0);
+    private Quaternion downward = Quaternion.Euler(180, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +19,11 @@ public class piranha : MonoBehaviour
     {
         if (gameObject.transform.position.y <= startPos.y && gameObject.GetComponent<Rigidbody2D>().velocity.y<0){
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up*force, ForceMode2D.Impulse);
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            gameObject.transform.rotation = upward;
         }
 
         if (gameObject.GetComponent<Rigidbody2D>().velocity.y < 0 && gameObject.transform.rotation.eulerAngles.x == 0){
-            gameObject.transform.rotation = Quaternion.Euler(180, 0, 0);
+            gameObject.transform.rotation = downward;
         }
         
     }
